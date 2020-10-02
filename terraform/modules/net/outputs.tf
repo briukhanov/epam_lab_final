2c5d3bc535689f141cfc66a00ca54f06b7287a0a
Briukhanov <a.briukhanov@gmail.com>
2020-10-02 23:11:58 +0300

    14

--- terraform/modules/net/outputs.tf
+++ terraform/modules/net/outputs.tf
@@ -1,9 +1,9 @@
 output "sg_id" {
   description = "Security Group ID"
-  value       = "${aws_security_group.web_security_group.id}"
+  value       = aws_security_group.web_security_group.id
 }
 
 output "public_subnet_id" {
   description = "Public Subnet ID"
-  value       = "${aws_subnet.public_subnet.id}"
+  value       = aws_subnet.public_subnet.id
 }
