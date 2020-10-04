363f2072afc012c5257fe9196fc42b3260c1c489
Briukhanov <a.briukhanov@gmail.com>
2020-10-05 00:20:01 +0300

    53

--- ansible/roles/build_dep/files/perl_lib.sh
+++ ansible/roles/build_dep/files/perl_lib.sh
@@ -27,4 +27,5 @@
 # apt install -y libdatetime-perl libmodule-find-perl libweb-scraper-perl libouch-perl libnumber-format-perl libperlio-gzip-perl libperl6-junction-perl liblist-moreutils-perl liblwp-perl libmodule-find-perl libmoose-perl libmoosex-role-withoverloading-perl libmoosex-types-perl libtext-csv_xs-perl liburi-perl libxml-parser-perlsax-perl libxml-dom-perl libtext-glob-perl libmoosex-followpbp-perl libmoosex-abc-perl libmoosex-fileattribute-perl libgetopt-std-perl liblog-handler-perl libdigest-md5-perl
 
 cpan DateTime Module::Find Web::Scraper Ouch Number::Format Perl6::Junction List::MoreUtils LWP Module::Find Moose MooseX::Role::WithOverloading MooseX::Types Text::CSV_XS URI XML::Parser::PerlSAX XML::DOM Text::Glob MooseX::FollowPBP MooseX::ABC MooseX::FileAttribute
-# PerlIO::gzip 
+# PerlIO::gzip
+apt install libperlio-gzip-perl
