dc1d01a5a2093399e1a0f280bf646d74ea07b538
Briukhanov <a.briukhanov@gmail.com>
2020-10-07 22:29:43 +0300

    72++++

--- terraform/webhook.tf
+++ terraform/webhook.tf
@@ -1,10 +1,10 @@
 
 resource "github_repository_webhook" "biotestmine" {
   repository = "intermine"
-  id         = "253428445"
-  url        = "https://api.github.com/repos/briukhanov/intermine/hooks/253428445"
-  active     = true
-  events     = ["push"]
+  # id         = "253428445"
+  # url        = "https://api.github.com/repos/briukhanov/intermine/hooks/253428445"
+  active = true
+  events = ["push"]
 
   configuration {
     url          = "http://${module.app_layer.docker_server_address}:8080/github-webhook/"
