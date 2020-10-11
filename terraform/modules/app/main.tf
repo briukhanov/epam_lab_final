804fef8699362a95976bd054b714a510bcf777db
Briukhanov <a.briukhanov@gmail.com>
2020-10-11 22:52:38 +0300

    enble commented strings

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -1,4 +1,4 @@
-
+# Instance for Application
 resource "aws_instance" "web_instance" {
   ami                    = lookup(var.ec2_images, var.aws_region)
   instance_type          = var.web_instance_type
@@ -11,46 +11,32 @@ resource "aws_instance" "web_instance" {
   volume_tags = {
     Name = "${(var.cluster_name)}_${(var.env_name)}_web_instance_volume"
   }
-  # provisioner "file" {
-  #   source      = "../ansible"
-  #   destination = "/tmp"
-  # }
-  # provisioner "remote-exec" {
-  #   inline = [
-  #     "sudo apt-add-repository -y ppa:ansible/ansible",
-  #     "sudo apt -y update",
-  #     "sudo apt -y aptitude",
-  #     "sudo apt -y install ansible",
-  #     "mv /tmp/ansible ~/",
-  #     "cd ~/ansible"
-  #   ]
-  #   # "sudo ansible-playbook site.yml --tags docker"
-  #   # Use "webapp" as a tags for instal Tomcat, Postgrsql, and Buil dependencies for web_app_instance
-  # }
-  # connection {
-  #   type        = "ssh"
-  #   user        = "ubuntu"
-  #   password    = ""
-  #   host        = self.public_ip
-  #   private_key = file(var.private_key_ec2)
-  # }
+  provisioner "file" {
+    source      = "../ansible"
+    destination = "/tmp"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-add-repository -y ppa:ansible/ansible",
+      "sudo apt -y update",
+      "sudo apt -y aptitude",
+      "sudo apt -y install ansible",
+      "mv /tmp/ansible ~/",
+      "cd ~/ansible",
+      "sudo ansible-playbook site.yml --tags docker --extra-vars \"docker_user=${(var.docker_user)} docker_pwd=${(var.docker_pwd)} env_name=${(var.env_name)}\""
+    ]
+    # Use "webapp" as a tags for instal Tomcat, Postgrsql, and Buil dependencies for web_app_instance
+  }
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    password    = ""
+    host        = self.public_ip
+    private_key = file(var.private_key_ec2)
+  }
 }
 
-
-# data "template_file" "init_docker_instance" {
-#   template = file("init_docker_inst.sh.tpl")
-#   vars = {
-#     dock_user = var.docker_user
-#     dock_pwd  = var.docker_pwd
-#   }
-# }
-#
-# resource "null_resource" "export_rendered_template" {
-#   provisioner "local-exec" {
-#     command = "cat > test_output.sh <<EOL\n${data.template_file.init_docker_instance.rendered}\nEOL"
-#   }
-# }
-
+# Instance for Jenkins
 resource "aws_instance" "docker_instance" {
   ami                    = lookup(var.ec2_images, var.aws_region)
   instance_type          = var.docker_instance_type
@@ -74,20 +60,11 @@ resource "aws_instance" "docker_instance" {
       "sudo apt -y update",
       "sudo apt -y aptitude",
       "sudo apt -y install ansible",
-      "sudo ansible --version",
-      # "cd ~",
-      # "git clone https://github.com/briukhanov/epam_lab_final.git",
       "mv /tmp/ansible ~/",
       "cd ~/ansible",
       "sudo ansible-playbook site.yml --tags docker --extra-vars \"docker_user=${(var.docker_user)} docker_pwd=${(var.docker_pwd)} env_name=${(var.env_name)}\""
-      #"sudo docker login -u ${(var.docker_user)} -p ${(var.docker_pwd)} > login.txt",
-      #"sudo docker run --name Jenkins -d -p 8080:8080 -p 50000:50000 wibob/intermine_dev_jenk:${(var.env_name)}",
-      #"sudo docker logout"
     ]
   }
-  # "'sudo ansible-playbook site.yml --tags docker --extra-vars \"docker_user=${(var.docker_user)}\"'",
-  # --extra-vars '{"docker_user":"$DOCKER_USER","docker_pwd":"$DOCKER_PWD"}'
-  # --extra-vars "version=1.23.45 other_variable=foo"
 
   connection {
     type        = "ssh"
@@ -95,22 +72,12 @@ resource "aws_instance" "docker_instance" {
     password    = ""
     host        = self.public_ip
     private_key = file(var.private_key_ec2)
-    # private_key = var.aws_ec2_key
   }
-
-  # depends_on = [
-  #   local_file.tf_ansible_vars_file_new
-  # ]
 
 }
 
+# Generating file with instance addresses (only for information)
 
-# resource "null_resource" "getting_pass" {
-#   provisioner "local-exec" {
-#     command = "ssh -o StrictHostKeyChecking=no -i ${var.private_key_ec2} ubuntu@${aws_instance.docker_instance.public_dns} sudo docker exec Jenkins cat /var/jenkins_home/secrets/initialAdminPassword"
-#     # >> ../ansible/jenkins_init.txt"
-#   }
-# }
 resource "local_file" "tf_ansible_vars_file_new" {
   content  = <<-DOC
     # Ansible vars_file containing variable values from Terraform.
@@ -125,3 +92,24 @@ resource "local_file" "tf_ansible_vars_file_new" {
     aws_instance.docker_instance
   ]
 }
+
+# data "template_file" "init_docker_instance" {
+#   template = file("init_docker_inst.sh.tpl")
+#   vars = {
+#     dock_user = var.docker_user
+#     dock_pwd  = var.docker_pwd
+#   }
+# }
+#
+# resource "null_resource" "export_rendered_template" {
+#   provisioner "local-exec" {
+#     command = "cat > test_output.sh <<EOL\n${data.template_file.init_docker_instance.rendered}\nEOL"
+#   }
+# }
+
+# resource "null_resource" "getting_pass" {
+#   provisioner "local-exec" {
+#     command = "ssh -o StrictHostKeyChecking=no -i ${var.private_key_ec2} ubuntu@${aws_instance.docker_instance.public_dns} sudo docker exec Jenkins cat /var/jenkins_home/secrets/initialAdminPassword"
+#     # >> ../ansible/jenkins_init.txt"
+#   }
+# }
