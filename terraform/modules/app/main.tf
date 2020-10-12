372fb8d62f296e1573e3dc061a400c7d1734fc5a
Briukhanov <a.briukhanov@gmail.com>
2020-10-12 09:44:52 +0300

    203

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -103,11 +103,16 @@ data "template_file" "init_jenkins" {
   ]
 }
 
-# resource "null_resource" "export_rendered_template" {
-#   provisioner "local-exec" {
-#     command = "cat > jenkins-set.groovy <<EOL\n${data.template_file.init_jenkins}\nEOL"
-#   }
-# }
+output "jenkins-set.groovy" {
+  value = data.template_file.init_jenkins.rendered
+}
+
+
+resource "null_resource" "export_rendered_template" {
+  provisioner "local-exec" {
+    command = "cat > jenkins-set.groovy <<EOL\n${data.template_file.init_jenkins.rendered}\nEOL"
+  }
+}
 
 # resource "null_resource" "getting_pass" {
 #   provisioner "local-exec" {
